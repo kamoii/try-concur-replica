3dd2f88f75eef3f820bbb5d61901870a5d49a641
kamoii <>
2019-07-04 04:13:53 +0900

    fix

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import P
 
 main :: IO ()
-main = someFunc
+main = putText "yeah"
---
+++ src/P.hs
@@ -0,0 +1,6 @@
+module P
+  ( module P
+  , module Relude
+  ) where
+
+import Relude hiding (dev)
