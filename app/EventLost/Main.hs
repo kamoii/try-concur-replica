a11d686bcac7142945a1c9a684383b3b4a511e27
kamoii <>
2019-07-17 10:47:45 +0900

    foo

--- app/EventLost/Main.hs
+++ app/EventLost/Main.hs
@@ -12,26 +12,31 @@ import           Control.Exception
 import           Concur.Core
 import           Concur.Replica
 
-counter :: Int -> Widget HTML void
-counter i = do
+{-|
+高速にカウントする自動カウンタと手動カウンタを同時表示したら、
+Event lost問題を再現できるかと思ったが、かなり高速にカウントしても
+問題なかった。
+
+ということは、気にするほどでもないってことか。僅かな可能性で発生したとしても、
+ユーザは「おや？」と思うぐらいか。
+-}
+
+manualCounter :: Int -> Widget HTML void
+manualCounter i = do
   div []
     [ text $ "counter: " <> show i
     , button [ onClick ] [ text "inc" ]
     ]
-  counter $ i+1
+  manualCounter $ i+1
 
-
-{-
-実装の問題で時偶イベントを失なう
--}
-widget :: Int -> Widget HTML void
-widget i = do
-  liftIO (threadDelay $ 1 * 1000 * 1000) <|> div [] [ text $ show i ]
-  widget $ i+1
+autoCounter :: Int -> Widget HTML void
+autoCounter i = do
+  liftIO (threadDelay $ 1 * 1 * 1000) <|> div [] [ text $ show i ]
+  autoCounter $ i+1
 
 main :: IO ()
 main = do
   runDefault 8080 "test" $ orr
-    [ widget 0
-    , counter 0
+    [ autoCounter 0
+    , manualCounter 0
     ]
--- app/PhantomEvent/Main.hs
+++ app/PhantomEvent/Main.hs
@@ -12,27 +12,22 @@ import           Control.Exception
 import           Concur.Core
 import           Concur.Replica
 
-
-exWidget :: Widget HTML a
-exWidget = do
-  e <- div [ onClick, error "foo" ] [ text "BOOM" ]
-  -- a <- liftIO $ evaluate ((5 `P.div` 0))
-  text $ T.pack $ show "Done"
-  exWidget
+{-|
+ラグがある環境(例: throttle --rt 500 --localhost)だと、"go next"のボタンを二回押す、
+二回目のイベントが同じパスを持つ "a" ボタンに発火してしまい、"a" が自動的に選択される。
+これが "a" じゃなく、副作用を持つ処理の決定ボタンだと問題。
+-}
 
 main :: IO ()
 main = do
   runDefault 8080 "test" $ do
-    exWidget
-    -- div []
-    --   [ p [] [ text "hello" ]
-    --   , button [ onClick ] [ text "go next" ]
-    --   ]
-    -- c <- div []
-    --   [ p [] [ text "choose" ]
-    --   , div []
-    --     [ button [ "a" <$ onClick ] [ text "a" ]
-    --     , button [ "b" <$ onClick ] [ text "b" ]
-    --     ]
-    --   ]
-    -- p [] [ text $ "you choosed: " <> c ]
+    div []
+      [ p [] [ text "hello" ]
+      , button [ onClick ] [ text "go next" ]
+      ]
+    c <- div []
+      [ p [] [ text "choose" ]
+      , button [ "a" <$ onClick ] [ text "a" ]
+      , button [ "b" <$ onClick ] [ text "b" ]
+      ]
+    p [] [ text $ "you choosed: " <> c ]
