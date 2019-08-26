9cd88884fe96f561285fc4a4273e3b3e8a40fe99
kamoii <>
2019-08-26 16:04:07 +0900

    Fixed docker

--- docker.nix
+++ docker.nix
@@ -5,6 +5,10 @@ let
 in
   pkgs.dockerTools.buildImage {
     name = "spla-league-match";
-    contents = hsPkgs.try-concur-replica.components.exes.spla-league-random-match;
+    contents = [
+      hsPkgs.try-concur-replica.components.exes.spla-league-random-match
+      pkgs.iana-etc
+      pkgs.cacert
+    ];
     config.Cmd = [ "/bin/spla-league-random-match" ];
   }
